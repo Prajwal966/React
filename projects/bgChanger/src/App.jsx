3502ebdb0359586a7d96d0a985f8e54ae2472d0f
Prajwal <prajwalsoni75@gmail.com>
2024-06-30 04:08:03 +0530

    all my react projects

---
+++ projects/bgChanger/src/App.jsx
@@ -0,0 +1,13 @@
+import RandomPasswordGenerator from "./component/RandomPasswordGenerator";
+import BgChanger from "./component/BgChanger";
+
+function App() {
+  return (
+    <>
+      <RandomPasswordGenerator />
+      <BgChanger />
+    </>
+  );
+}
+
+export default App;
